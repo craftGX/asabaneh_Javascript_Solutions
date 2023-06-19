7f998082c05574375cc3f207208d90e8838d781e
stephane soussi <84611956+craftGX@users.noreply.github.com>
2023-06-19 17:01:01 +0200

    Add files via upload
    fin level 1

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-//solutions
+//solutions Level 1
 function fullName(name){
     console.log(name);
 }
@@ -58,4 +58,38 @@ function convertCelsiusToFahrenheit(oC){
     let oF = (oC*(9/5)) + 32;
     return oF;
 }
-console.log(convertCelsiusToFahrenheit(26));
+
+function BMI(weight,height){
+    let bmi = weight/(height*height);
+    if (bmi < 18.5){
+        console.log("you are Underweight");
+    }else if(18.5<bmi<24.9){
+        console.log("you are Normal weight")
+    }else if(25<bmi<29.9){
+        console.log("you are Overweight")
+    }else if(bmi>30){
+        console.log("you are Obese")
+    }   
+}
+
+function checkSeason(month){
+    if(month==="janvier" || month==="fevrier" || month==="mars"){
+        console.log("nous sommes en hiver");
+    }else if(month==="avril" || month==="mai" || month==="juin"){
+        console.log("nous sommes au printemps");
+    }else if(month==="juillet" || month==="aout" || month==="septembre"){
+        console.log("nous sommes en été");
+    }else if(month==="octobre" || month==="novembre" || month==="decembre"){
+        console.log("nous sommes en automne");
+    }
+}
+
+function findMax(a,b,c){
+    return Math.max(a,b,c);
+}
+console.log(findMax(1,2,3));
+
+//solutions Level 2
+function solveLinEquation(a,b,c){
+
+}
