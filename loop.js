6283bf1893d9796da4c5d219a1d8e97647b7e9cc
stephane soussi <84611956+craftGX@users.noreply.github.com>
2023-06-26 21:39:59 +0200

    Add files via upload
    travail du 26 juin 2023

---
+++ loop.js
@@ -0,0 +1,60 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  
+  const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+  // level 1
+  // exercice 1
+  /*for(let i =0;i<10;i++){
+    console.log(i);
+  }*/
+
+  /*let i =0;
+  while(i<10){
+    console.log(i);
+    i++;
+  }*/
+
+  /*let i = 0;
+  do{
+    console.log(i);
+    i++;
+  }while(i<10);*/
+
+  // exercice 4
+  /*for (let i = 1; i <= 7; i++) {
+    console.log("#".repeat(i));
+  }*/
+
+  // exercice 5
+  /*for(let i=0;i<10;i++){
+    console.log(i + "*" + i + "=" + (i*i));
+  }*/
+
+  // exercice 6
+  for(let i = 0;i<10;i++){
+    console.log(i + " " + (i*i) + " " + (i*i*i)); 
+  }
+  
+
